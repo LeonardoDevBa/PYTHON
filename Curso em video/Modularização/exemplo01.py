87a0cdda2d177525cfe3daaeab775da77242a645
Leonardo Machado Britto Junior <leo.dev2001@gmail.com>
2024-12-19 14:08:04 +0000

    Salvar

---
+++ Curso em video/Modularização/exemplo01.py
@@ -0,0 +1,11 @@
+from os import system
+system("cls||clear")
+
+def fatorial(n):
+    f = 1
+    for c in range (1,n+1):
+        f*=c
+    return f
+num = int(input("Digite um numero: "))
+fat = fatorial(num)
+print(f"O fatorial de {num} é {fat}")
